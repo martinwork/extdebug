d993510e6a707e187b7fe575587ef0bdb979086f
Martin Williams <martinwork@users.noreply.github.com>
2023-11-10 02:05:11 +0000

    Create a block in main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,14 @@
-basic.forever(function () {
-	
-})
+/**
+ * debugext blocks
+ */
+//% weight=100 color=#0fbc11 icon=""
+namespace debugext {
+    /**
+     * call showString
+     * @param s the text, eg: "ByeBye"
+     */
+    //% block
+    export function print( s: string): void {
+        basic.showString(s)
+    }
+}
